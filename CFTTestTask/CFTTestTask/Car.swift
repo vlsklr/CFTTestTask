0c0347e347d0a9e95e4fe746336cd5a3c6f77f02
vlsklr <42057101+vlsklr@users.noreply.github.com>
2020-09-16 23:01:17 +0700

    Add files via upload

---
+++ CFTTestTask/CFTTestTask/Car.swift
@@ -0,0 +1,36 @@
+//
+//  Car.swift
+//  CFTTestTask
+//
+//  Created by Vlad on 16.09.2020.
+//  Copyright © 2020 Vlad. All rights reserved.
+//
+
+import Foundation
+
+ class Car {
+    var manufacturerName: String?
+    var yearOfManufactoring: Int?
+    var modelName: String?
+    var bodyType: String?
+    
+    init() {
+        manufacturerName = "Mitsubishi"
+        yearOfManufactoring = 2008
+        modelName = "Galant"
+        bodyType = "Sedan"
+    }
+    init(manufacturerName: String) {
+        self.manufacturerName = manufacturerName
+        yearOfManufactoring = 2000
+        modelName = "Unknown"
+        bodyType = "Unknown"
+    }
+    init(manufacturerName: String, yearOfManufactoring: Int) {
+        self.manufacturerName = manufacturerName
+        self.yearOfManufactoring = yearOfManufactoring
+        modelName = "Unknown"
+        bodyType = "Unknown"        
+    }
+    
+}
---
+++ CFTTestTask/CFTTestTask/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  CFTTestTask
+//
+//  Created by Vlad on 16.09.2020.
+//  Copyright © 2020 Vlad. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var kek = readLine()
+
+print(kek)
+
+let car = Car()
+print(car.manufacturerName)
+print(car.bodyType)
