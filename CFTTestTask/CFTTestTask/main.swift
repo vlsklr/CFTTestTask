64cd8bd32746653e6e80fafc3e15643f2c7c5bba
Vlad <vlad_sklyarov@mail.ru>
2020-09-20 19:44:51 +0700

    Done

--- CFTTestTask/CFTTestTask/main.swift
+++ CFTTestTask/CFTTestTask/main.swift
@@ -9,16 +9,60 @@
 import Foundation
 var menuPosition: String? = "0"
 var carList = [Car]()
-let car = Car()
-print(car.manufacturerName)
-print(car.bodyType)
-print(car.modelName)
-carList.append(car)
 
+
+func loadDataFromFile() {
+    let manager = FileManager.default
+    do {
+        //Получаем список файлов в рабочей папке
+        let filesInDirectory = try manager.contentsOfDirectory(atPath: manager.currentDirectoryPath)
+        //Проверяем наличие искомого
+        if (filesInDirectory.contains("carBase.txt")){
+            //Получаем и разделяем на массив строк
+           let contntsOfFile = try NSString(contentsOfFile: manager.currentDirectoryPath + "/carBase.txt", encoding: String.Encoding.utf8.rawValue)
+            let tempArrayOfCars = contntsOfFile.components(separatedBy: "\n")
+            for item in tempArrayOfCars {
+                //Разбиваем каждую строку массива на значения, создаем экземпляр машины и добавляем в массив
+                    let currentCar = item.components(separatedBy: ",")
+                    if (currentCar.count == 4) {
+                        carList.append(Car(manufacturerName: currentCar[0], yearOfManufactoring: Int(currentCar[2]) ?? 1900, modelName: currentCar[1], bodyType: currentCar[3]))
+                    
+                }
+            }
+        }
+    }
+    catch {
+        print("Something wrong: \(error)")
+    
+    }
+}
+
+func saveDataToFile() {
+    //формируем адрес сохранения данных
+    let fileUrl = FileManager.default.currentDirectoryPath + "/carBase.txt"
+    var resultString: String = ""
+    //приводим каждую строку массива машин к нужному виду
+    for item in carList {
+        var tmpString = item.manufacturerName + ","
+        tmpString += item.modelName + ","
+        tmpString += String(item.yearOfManufactoring) + ","
+        tmpString += item.bodyType + "\n"
+        resultString += tmpString
+        
+    }
+    //пытаемся записать в файл. Файл каждый раз перезаписывается
+    do {
+        try resultString.write(toFile: fileUrl, atomically: false, encoding: .utf8)
+    }
+    catch {
+        print("Something wrong during writingFile \(error)")
+    }
+}
+// функция проверяет метод на валидность - машина может быть выпущена с 1806 года и до текущего
 func validateYearOfManufactoring(yearOfManu: Int) -> Bool {
     let calendar = Calendar.current
     let currentYear = calendar.component(.year, from: Date())
-    if (yearOfManu >= 1870 && yearOfManu <= currentYear){
+    if (yearOfManu >= 1806 && yearOfManu <= currentYear){
         return true
     }
     else
@@ -26,7 +70,7 @@ func validateYearOfManufactoring(yearOfManu: Int) -> Bool {
         return false
     }
 }
-
+//Функция выводит список машин в консоль
 func printCarList(){
     guard carList.count > 0 else {
         print("Car list is empty \(carList.count)")
@@ -38,14 +82,14 @@ func printCarList(){
         id += 1
     }
 }
-
+// Функция добавляет новый автомобиль, если при вводе какого либо года что-то пошло не так значение по умолчанию "Unknown"
 func addNewCar() {
     print("Insert a manufactorer name: ")
-    let manufactorerName = readLine()
+    let manufactorerName = readLine() ?? "Unknown"
     print("Insert a model name")
-    let modelName = readLine()
+    let modelName = readLine() ?? "Unknown"
     print("Insert a body type")
-    let bodyType = readLine()
+    let bodyType = readLine() ?? "Unknown"
     print("Insert a year of manufactoring: ")
     var validateStatus = false
     var manufactoringYear = 0
@@ -57,10 +101,10 @@ func addNewCar() {
         }
     }while(!validateStatus)
     
-    let newCar = Car(manufacturerName: manufactorerName ?? "Unknown", yearOfManufactoring: manufactoringYear, modelName: modelName ?? "Unknown", bodyType: bodyType ?? "Unknown")
+    let newCar = Car(manufacturerName: manufactorerName, yearOfManufactoring: manufactoringYear, modelName: modelName , bodyType: bodyType )
     carList.append(newCar)
 }
-
+//Функция удаления автомобиля. Проверяет наличие в массиве и удаляет
 func deleteCar(id : Int) {
     guard id > 0 && id <= carList.count else {
         print("ID is invalid")
@@ -68,30 +112,80 @@ func deleteCar(id : Int) {
     }
     carList.remove(at: id-1)
 }
-
+//Обновляет поля на выбор
 func updateCar() {
-    //carList[0].bodyType = 0
+    //выбор изменяего авто
     print("Insert a car ID:")
-    
+    var validatedID = false
+    var carID = 0
+    repeat {
+        carID = Int(readLine()!) ?? 0
+        if carID > 0 && carID <= carList.count {
+            validatedID = true
+        }
+        else {
+            print("ID is invalid! Try again: ")
+        }
+    }
+    while (!validatedID)
+    var menuPosition = "0"
+    //выбор и изменение поля
+    repeat {
+        print("Select position to change:\n","1. Body type\n","2. Manufacturer name\n","3. Model name\n", "4. Year of manufactoring\n","0 or any other key to exit")
+        menuPosition = readLine() ?? "0"
+        switch menuPosition {
+        case "1":
+            print("Insert new bodytype")
+            carList[carID-1].bodyType = readLine() ?? "Unknown"
+        case "2":
+            print("Insert new manufacturer name")
+            carList[carID-1].manufacturerName = readLine() ?? "Unknown"
+        case "3":
+            print("Insert new model name")
+            carList[carID-1].modelName = readLine() ?? "Unknown"
+        case "4":
+            var validatedYear = false
+            print("Insert new year of manufactoring:")
+            repeat {
+                let yearOfManu = Int(readLine()!) ?? 0
+                validatedYear = validateYearOfManufactoring(yearOfManu: yearOfManu)
+                if (!validatedYear) {
+                    print("Year is invalid. Try again:")
+                }else {
+                    carList[carID - 1].yearOfManufactoring = yearOfManu
+                }
+            } while (!validatedYear)
+        default:
+            menuPosition = "0"
+        }
+    }while(menuPosition != "0")
 }
+
+loadDataFromFile()
 repeat {
   print("Select menu item:")
     print(" 1.Show cars list.\n",
   "2.Add new car.\n",
   "3.Delete car from list.\n",
   "4.Change info about car from list\n",
-  "0.Exit")
+  "0 or any other key to exit")
   menuPosition = readLine()
   switch menuPosition {
   case "1":
     printCarList()
   case "2":
     addNewCar()
+    saveDataToFile()
   case "3":
     print("Insert ID:")
     deleteCar(id: Int(readLine()!) ?? 0)
+    saveDataToFile()
+  case "4":
+    updateCar()
+    saveDataToFile()
   default:
-    print("default")
+    print("Good Bye!")
+    menuPosition = "0"
 
   }
 } while (menuPosition != "0")
